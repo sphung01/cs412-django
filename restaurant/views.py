d43f48c0648cea1fa5d6200801dd7a844eac2bcf
Steven Phung <sphung01@bu.edu>
2025-05-22 22:11:23 -0400

    Completed a good part of confirmation page

--- restaurant/views.py
+++ restaurant/views.py
@@ -52,7 +52,7 @@ def get_ready_time():
 
     # We will then convert the 'progress_time' (seconds)
     # to an actual time.
-    ready_time = time.ctime(progress_time)
+    ready_time = time.strftime("%H:%M:%S %p", time.localtime(progress_time))
 
     return ready_time
 
@@ -92,7 +92,7 @@ def order(request):
     template = 'restaurant/order.html'
 
     context = {
-        'daily_special_item': get_daily_special()
+        'daily_special_item': get_daily_special() # Calls for the random daily item
     }
 
     return render(request, template, context)
@@ -114,11 +114,73 @@ def confirmation(request):
     # read the form data into python variables:
     if request.POST:
 
+        # Initializing the lists and total_cost of the order.
+
+        # Both combo_items and side_items create a list on inputs
+        # that have the same name, either 'combo' or 'side', 
+        # by using request.POST.getlist().
+        combo_items = request.POST.getlist('combo') 
+        side_items = request.POST.getlist('side')
+
+        # We will for loop and add the price to the total
+        total_cost = 0 
+
+        # This list will hold all the chosen items from the client
+        chosen_items = []
+
+        # For every item in the 'combo_items' list...
+        for item in combo_items:
+
+            # First, split the string into (name of item) | (price of item)
+            name, price = item.split('|') # e.g Krabby Patty|2.00
+
+            # Second, convert the string of the price into a float value
+            item_price = float(price)
+
+            # Third, add the price to the total
+            total_cost += item_price
+
+            # Lastly, append the name of the item to the 'chosen_items' list.
+            # Then, repeat process with other items
+            chosen_items.append(name)
+
+        # Just like the 'combo_items' for loop, it is the same concept
+        for appetizer in side_items:
+            name, price = appetizer.split('|')
+            appetizer_price = float(price)
+            total_cost += appetizer_price
+            chosen_items.append(name)
+
+        # We use request.POST.get() to avoid the error.
+        # Will return 'None' if the client did not check
+        # the daily item. Otherwise...
+        chosen_daily_item = request.POST.get('daily_item')
+
+        # If the client DID check for daily item
+        # We run the splitting name and price procedure
+        # just like 'side_items' and 'combo_items'
+        if chosen_daily_item:
+            name, price = chosen_daily_item.split('|')
+            chosen_daily_item_price = float(price)
+            total_cost += chosen_daily_item_price
+            chosen_items.append(name)
+
+        # Special Instructions given from the client
+        special_instructions = request.POST['special_instructions']
+
+        # Information given from the client
         name = request.POST['name']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
 
         context = {
             'name': name,
+            'email': email,
+            'phone_number': phone_number,
             'expected_time': get_ready_time(),
+            'total_cost': f"{total_cost:.2f}",
+            'chosen_items': chosen_items,
+            'special_instructions': special_instructions,
         }
 
     return render(request, template, context)
